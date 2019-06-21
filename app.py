867ec90e58584d71e92159a843ca9e5f9c4bdb70
rashi <rashikrishna16@gmail.com>
2019-06-21 21:53:48 +0530

    Sessions

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from flask_session import Session
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config["SESSION_PERMANENT"]=False
+app.config["SESSION_TYPE"]="filesystem"
+Session(app)
+
+@app.route("/", methods=["POST"])
 def index():
 	#return "Hello"
-	name="xyz";
-	return render_template("index.html",name=name)
+	session["user"]="new user"
+	session["user"]=request.form.get("userid")
+	return render_template("index.html",user=session["user"])
 
 @app.route("/about")
 def about():
@@ -15,6 +21,6 @@ def about():
 @app.route("/signin")
 def signin():
 	return render_template("form.html")
-	
+
 if (__name__ == "__main__"):
 	app.run(port = 5000)
