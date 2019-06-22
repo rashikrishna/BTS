3ed2b9608d74dd2c3cbfe9a3546728571c0c22ad
rashi <rashikrishna16@gmail.com>
2019-06-22 22:43:34 +0530

    Added Login Page

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
-from flask import Flask, render_template, session
-from flask_session import Session
+from flask import Flask, render_template, request
+#from flask_session import Session
+import os
 
 app = Flask(__name__)
 
-app.config["SESSION_PERMANENT"]=False
-app.config["SESSION_TYPE"]="filesystem"
-Session(app)
+#app.config["SESSION_PERMANENT"]=False
+#app.config["SESSION_TYPE"]="filesystem"
+#Session(app)
+#app.secret_key=os.urandom(24)
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["POST","GET"])
 def index():
-	#return "Hello"
-	session["user"]="new user"
-	session["user"]=request.form.get("userid")
-	return render_template("index.html",user=session["user"])
+	if request.method=="POST" :
+		#session["user"]="new user"
+		user="New User"
+		if requst.form.get("pwd")=="password" :
+			#session["user"]=request.form.get("userid")
+			user=request.form["userid"]
+			return render_template("index.html",user=user)
+
+	return render_template("index.html",user=user)
+
 
 @app.route("/about")
 def about():
@@ -20,7 +28,9 @@ def about():
 
 @app.route("/signin")
 def signin():
+
 	return render_template("form.html")
+
 
 if (__name__ == "__main__"):
 	app.run(port = 5000)
