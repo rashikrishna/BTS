16efac5ea3168f6b3c939f23548c7506cc038159
rashi <rashikrishna16@gmail.com>
2019-06-27 21:16:52 +0530

    Session Added

--- app.py
+++ app.py
@@ -1,36 +1,44 @@
 from flask import Flask, render_template, request
 #from flask.ext.session import Session
-from Flask-Session import Session
+#from flask_session import Session
 #import os
 
 app = Flask(__name__)
 
-app.config["SESSION_PERMANENT"]=False
-app.config["SESSION_TYPE"]="filesystem"
-Session(app)
+#app.config["SESSION_PERMANENT"]=False
+#app.config["SESSION_TYPE"]="filesystem"
+#Session(app)
 #app.secret_key=os.urandom(24)
+user="New User"
 
 @app.route("/", methods=["POST","GET"])
 def index():
-	if request.method=="POST" :
-		#session["user"]="new user"
-		user="New User"
-		if requst.form.get("pwd")=="password" :
-			#session["user"]=request.form.get("userid")
-			user=request.form["userid"]
-			return render_template("index.html",user=user)
-
+#	if request.method=="POST":
+#		if request.form.get("userpwd")=="password":
+#				user=request.form.get("userid")
+#				return render_template("index,html",user=user)
+	#session["user"]=request.form.get("userid")
 	return render_template("index.html",user=user)
 
+	#return render_template("index.html",user=user)
 
-@app.route("/about")
+@app.route("/index.html")
+def index1():
+	#session["user"]=request.form.get("userid")
+	return render_template("index.html",user=user)
+
+@app.route("/about.html")
 def about():
-	return render_template("form1.html")
+	return render_template("about.html")
 
-@app.route("/signin")
+@app.route("/signin.html")
 def signin():
+	return render_template("signin.html")
 
-	return render_template("form.html")
+@app.route("/members.html")
+def members():
+	return render_template("members.html")
+
 
 
 if (__name__ == "__main__"):
